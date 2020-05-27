7d295a95345311caff48b63210927324111aa24a
Rajasekhar Gattu <rajasekhargattu@gmail.com>
2020-05-27 17:40:35 +0530

    Updates

---
+++ precache-manifest.ef6c0628aef87d1d3d02a8c0607c3ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53505bb3fa781c75a9e2ceca1489b3d0",
+    "url": "/kyc-app/login/index.html"
+  },
+  {
+    "revision": "374d8eaeba2434131ba7",
+    "url": "/kyc-app/login/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ce88a2152a8fe75d6bac",
+    "url": "/kyc-app/login/static/js/2.fc40a1c3.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/kyc-app/login/static/js/2.fc40a1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374d8eaeba2434131ba7",
+    "url": "/kyc-app/login/static/js/main.0c5d34c8.chunk.js"
+  },
+  {
+    "revision": "0f06145cea2c073442cf",
+    "url": "/kyc-app/login/static/js/runtime-main.f891bbe1.js"
+  }
+]);
